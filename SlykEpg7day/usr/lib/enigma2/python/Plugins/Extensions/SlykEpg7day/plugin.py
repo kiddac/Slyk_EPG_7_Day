0684d9566ae9e51b55bae23e4898ad3b1e9e30c9
kiddac <kiddac2015@gmail.com>
2026-06-19 19:22:48 +0100

    updated user-agent

--- SlykEpg7day/usr/lib/enigma2/python/Plugins/Extensions/SlykEpg7day/plugin.py
+++ SlykEpg7day/usr/lib/enigma2/python/Plugins/Extensions/SlykEpg7day/plugin.py
@@ -33,8 +33,7 @@ sEautoStartTimer = None
 
 
 hdr = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36',
-    'Accept-Encoding': 'gzip, deflate'
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/145.0.0.0 Safari/537.36',
 }
 
 regionlist = [
